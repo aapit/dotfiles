f575c08fb2eb0e19163d06e24c3b51c18a422a35
David Spreekmeester <david@grrr.nl>
2020-07-22 17:16:53 +0200

    Cleaned up aliases

--- Scripts/env/aliases.sh
+++ Scripts/env/aliases.sh
@@ -39,8 +39,6 @@ alias vim-color-edit='nvim ~/.vim/colors/spacemonkey_dark.vim'
 alias c="xclip -selection c"
 
 # Dirs
-#alias ..="cd .."
-#alias ...="cd ../.."
 alias desk="cd ~/Desktop"
 alias ~="cd ~"
 alias dotfiles="cd ~/.dotfiles/"
@@ -53,9 +51,9 @@ alias config-edit-vim-theme="(cd $HOME/.config/vim/colors && vim spacemonkey_dar
 alias config-edit-cloudkey="(cd $HOME/Remotes/unifi-cloudkey && vim config.gateway.json && make && echo 'Now adjust a setting in the Cloudkey UI, like Local Network dhcp range, to trigger provisioning to the Security Gateway.' && xdg-open "https://cloudkey.floase.zone:8443/manage/site/default/v2/settings/networks/manage/edit/5d45683ee5c66d0dee9b68d1" && cd -)"
 alias config-edit-ha="(cd $HOME/Remotes/domo && make mount-and-symlink && nvim ./mount/configuration.yaml)"
 alias config-edit-alias="alias-edit"
-alias config-edit-path="vim ~/Scripts/dotfiles/shell/env/paths.sh && . ~/Scripts/dotfiles/shell/env/paths.sh"
+alias config-edit-path="vim ~/Scripts/env/paths.sh && . ~/Scripts/env/paths.sh"
 alias config-edit-zsh="vim ~/.zshrc"
-alias config-edit-profile="vim ~/Scripts/dotfiles/shell/.profile && . ~/.profile"
+alias config-edit-profile="vim $ZDOTDIR/.zprofile && . $ZDOTDIR/.zprofile"
 
 # Python
 alias py='python'
@@ -76,10 +74,6 @@ alias dev='git co develop'
 alias master='git co master'
 alias git-push-upstream='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
 
-# iTerm profile switching, tmux proof
-#alias light='. ~/Scripts/interface/switch-to-light.sh'
-#alias dark='. ~/Scripts/interface/switch-to-dark.sh'
-
 # Docker
 alias dco='docker-compose'
 alias dce='docker-compose exec'
@@ -90,26 +84,9 @@ alias docker-snafu='~/Scripts/docker/docker-snafu.sh'
 # Kubernetes
 alias k='kubectl'
 
-# Todoist CLI
-alias t='todoist'
-alias to='todoist l -f "(overdue | today)"'
-
 # tmux
 #alias tmux='tmux -2'
-#alias tmux='TERM=screen-256color-bce tmux -2'
 alias rn='tmux movew -r && tmux rename-window'
-
-# Packages
-alias apt-edit-pkgs="vim $HOME/Scripts/dotfiles/package-managers/apt/apt-packages.txt"
-alias apt-install-pkgs=". $HOME/Scripts/dotfiles/package-managers/apt/apt-install.sh"
 
 # Spotify
 alias spotify="$(which spotify) --force-device-scale-factor=2 > /dev/null &"
-
-# Temp disabled / up for archival
-# _______________________________
-
-# Tasks
-#alias t='task'
-#alias tl='task list'
-#alias ta='task add'
