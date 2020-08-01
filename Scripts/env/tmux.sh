3b362d31c1c5a195d589f07e42261a66d404d04d
David Spreekmeester <david@grrr.nl>
2020-08-01 13:35:14 +0200

    Cleaned up tmux config, improved auto-start

--- Scripts/env/tmux.sh
+++ Scripts/env/tmux.sh
@@ -1,4 +1,6 @@
 export TMUX_CONFIG_PATH=$HOME/.config/tmux/
 export TMUX_PLUGIN_MANAGER_PATH=$HOME/.config/tmux/plugins/
 
-#[ -z "$TMUX" ] && exec tmux -2 new -As 家に
+if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
+    tmux attach || tmux new -As 家に
+fi
