c1e5431454a503c4e5a7c611e72d8e81c8d2607b
David Spreekmeester <david@grrr.nl>
2020-08-01 17:22:01 +0200

    Optimized tmux nesting

--- Scripts/env/tmux.sh
+++ Scripts/env/tmux.sh
@@ -2,5 +2,5 @@ export TMUX_CONFIG_PATH=$HOME/.config/tmux/
 export TMUX_PLUGIN_MANAGER_PATH=$HOME/.config/tmux/plugins/
 
 if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
-    tmux attach || tmux new -As 家に
+    tmux new -As $(hostname);
 fi
