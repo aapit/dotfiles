0e416afb6bec29e6085c7721d0f41d6630ca373d
David Spreekmeester <david@grrr.nl>
2020-08-19 01:52:08 +0200

    Added super-agenda and plantUML modules for Org

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -7,6 +7,8 @@
 (package! org-roam-server)
 (package! emojify)
 (package! gnus-alias)
+(package! org-super-agenda)
+(package! plantuml-mode)
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
