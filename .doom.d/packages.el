c4d5c472fbb8a37396d9702bd4bde64181fa8189
David Spreekmeester <david@grrr.nl>
2020-09-06 15:55:54 +0200

    Added Slack package for Doom

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -9,6 +9,7 @@
 (package! gnus-alias)
 (package! org-super-agenda)
 (package! plantuml-mode)
+(package! slack)
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
