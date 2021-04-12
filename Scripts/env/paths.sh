fb212cab344540d0ebfa1fc2fa373416c8bf63e9
David Spreekmeester <david@grrr.nl>
2021-04-12 16:32:16 +0200

    Added Cuda and Python paths

--- Scripts/env/paths.sh
+++ Scripts/env/paths.sh
@@ -1,7 +1,7 @@
 #export GOPATH=/usr/local/opt/golang
 #export GOROOT=/usr/local/opt/go/libexec
 #export GEM_HOME=$HOME/.gem
-export PYTHONPATH="$HOME/.local/lib/python3.9/site-packages:/usr/lib/python3.9/dist-packages:$HOME/.local/lib/tensorflow-model-garden/research"
+export PYTHONPATH="$HOME/.local/lib/python3.9/site-packages:/usr/lib/python3.9/site-packages:/usr/lib/python3.9/dist-packages:$HOME/.local/lib/tensorflow-model-garden/research"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
@@ -12,6 +12,7 @@ export EMACSCLIENT="/snap/bin/emacsclient -c -n -s /run/user/1000/snap.emacs/ema
 #export KUBECONFIG="$HOME/.kube/config"
 export TF_IMG=$([[ `~/Scripts/display/has-gpu.sh` == '1' ]] && echo 'tensorflow/tensorflow:latest-gpu-jupyter' || echo 'tensorflow/tensorflow:latest-jupyter')
 export CUDA_BIN_PATH=/opt/cuda/bin
+export LIBRARY_PATH=/opt/cuda/lib64
 export EDITOR=vim
 export VISUAL=vim
 export GIT_EDITOR=vim
