a37dd5130fb476807f3d70a40b5dbe12f3ff8855
David Spreekmeester <david@grrr.nl>
2023-01-07 13:31:45 +0100

    Update to Roam v2

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -127,7 +127,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam +hugo +pandoc +pretty +gnuplot) ; organize your plain life in plain text
+       (org +roam2 +hugo +pandoc +pretty +gnuplot) ; organize your plain life in plain text
        ;;(org +roam +pomodoro +present +hugo +pandoc +pretty)
        php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
