a7541a9b0eb9c8f210b4341446e7b3611bef6c49
David Spreekmeester <david@grrr.nl>
2023-01-26 09:06:51 +0100

    Add pastelpunk color tweaks on top of Horizon theme

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -13,4 +13,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(italic ((t (:foreground "medium sea green" :slant italic)))))
+ '(italic ((t (:foreground "medium slate blue" :slant italic))))
+ '(org-date ((t (:foreground "chocolate"))))
+ '(org-document-info-keyword ((t (:inherit shadow :foreground "dim gray"))))
+ '(org-document-title ((t (:background "gray11" :foreground "#b877db" :weight bold :height 1.3 :family "FantasqueSansMono Nerd Font"))))
+ '(org-link ((t (:inherit underline :foreground "chocolate"))))
+ '(org-meta-line ((t (:foreground "dim gray"))))
+ '(org-property-value ((t (:foreground "light gray"))) t)
+ '(org-special-keyword ((t (:foreground "dim gray"))))
+ '(org-tag ((t (:foreground "hot pink" :weight normal)))))
